11f7fc2572d034c92cf1acb4e2b5afa8bfaa2dde
TatsianaLohinava <tlohinava@solvd.com>
2023-03-15 16:08:00 +0300

    Test data was moved to file

--- test/specs/main.spec.js
+++ test/specs/main.spec.js
@@ -1,11 +1,16 @@
 const MainPage = require('../pageobjects/main.page');
 const SearchBar = require('../pageobjects/search.page');
 const LoginForm = require('../pageobjects/login.page');
+const fs = require('fs');
+
+const testData = fs.promises.readFile('test/data/test-data.json', 'utf8')
+    .then((data) => JSON.parse(data));
 
 describe('Main page', () => {
     it('resets the search input', async () => {
+        const data = await testData;
         await MainPage.open();
-        await SearchBar.searchField.addValue('Ластик');
+        await SearchBar.searchField.addValue(data.query);
         await SearchBar.resetButton.click();
         await expect(SearchBar.searchField).toHaveValue('')
     })
@@ -20,10 +25,11 @@ describe('Main page', () => {
     })
 
     it('shows message on invalid login credentials', async () => {
+        const data = await testData;
         await MainPage.open();
         await MainPage.loginButton.click();
         await LoginForm.loginWithEmailButton.click();
-        await LoginForm.login('abcd@gmail.com', 'abcd');
+        await LoginForm.login(data.email, data.pass);
         await expect(LoginForm.form).toHaveTextContaining('Адрес электронной почты не зарегистрирован.')
     })
 
--- test/specs/result.spec.js
+++ test/specs/result.spec.js
@@ -1,18 +1,24 @@
 const MainPage = require('../pageobjects/main.page');
 const SearchBar = require('../pageobjects/search.page');
 const ResultPage = require('../pageobjects/result.page');
+const fs = require('fs');
+
+const testData = fs.promises.readFile('test/data/test-data.json', 'utf8')
+    .then((data) => JSON.parse(data));
 
 describe("Result page", () => {
     it('checks the query results', async () => {
+        const data = await testData;
         await MainPage.open();
-        await SearchBar.searchField.setValue('Сыворотка');
+        await SearchBar.searchField.setValue(data.query);
         await SearchBar.searchButton.click();
         await ResultPage.checkProducts('.item-type-card__title');
     })
 
     it('filters the new products', async () => {
+        const data = await testData;
         await MainPage.open();
-        await SearchBar.searchField.setValue('Bitey');
+        await SearchBar.searchField.setValue(data.query);
         await SearchBar.searchButton.click();
         await ResultPage.filterOptionForNew.click();
         await ResultPage.checkProducts('span*=Новинка');
